f0246fc047dc8779bcef85d678ec1e60d3771fe0
student239 <vAphG2PjmWXuNfVvHpcu>
2022-04-09 09:11:25 +0300

    скам не работает

--- src/Action.java
+++ src/Action.java
@@ -15,11 +15,21 @@ public class Action {
         return "Bulls: "+count+"\n"+"Cows: "+ (4-count);
     }
 
+    public int action1(int n, int k){
+        int count = 0;
+        for (int i = 1; i <= 4; i++) {
+            if ((n % (int)(Math.pow(10,i)))/((int)(Math.pow(10,i-1))) == (k % (int)(Math.pow(10,i)))/((int)(Math.pow(10,i-1)))){
+                ++count;
+            }
+        }
+        return count;
+    }
+
     public int findNumber(int count, int limit, int n){
-        int number = 0;
+        int number = -1;
 
         for (int k = limit; k <10000 ; k++) {
-            int count1=0;
+            int count1 = 0;
             for (int i = 1; i <= 4; i++) {
                 if ((n % (int)(Math.pow(10,i)))/((int)(Math.pow(10,i-1))) == (k % (int)(Math.pow(10,i)))/((int)(Math.pow(10,i-1)))){
                     ++count1;
@@ -34,7 +44,20 @@ public class Action {
 
     }
 
-    /*public String scam(ArrayList<Integer> cN, ArrayList<Integer> aN){
+    public int scam(ArrayList<Integer> cN, ArrayList<Integer> aN){
+        int x = 0;
+        for (int i = 1000; i <10000 ; i++) {
+            int n = findNumber(cN.get(cN.size()-1),i,aN.get(aN.size()-1));
+            for (int j = 0; j < aN.size(); j++) {
+                if (j == aN.size() -1) x = n;
+                if (cN.get(j) == action1(n, aN.get(j))){
+                    continue;
+                } else {
+                    break;
+                }
 
-    }*/
+            }
+        }
+        return x;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,26 +2,36 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
+
 public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> actionNumber = new ArrayList();
-
-        Scanner scanner = new Scanner(System.in);
-        Action actor = new Action();
+        Action action = new Action();
         Random random = new Random();
         int n = 1234;//random.nextInt(8999) + 1000;
         int k = 0;
-        while (n!=k) {
-            k = scanner.nextInt();
-            actionNumber.add(actionNumber.size(), k);
-            System.out.println(actor.action(n,k));
-            System.out.println(actionNumber);
-            System.out.println(actor.countNumber);
-
-        }
-        if (n==k){
+        play(n, k, actionNumber, action);
+        if (action.scam(action.countNumber, actionNumber) == 0 || action.scam(action.countNumber, actionNumber) == n) {
             actionNumber.clear();
             System.out.println("You win!");
+        } else {
+            n = action.scam(action.countNumber, actionNumber);
+        }
+        play(n, k, actionNumber, action);
+
+    }
+
+    private static void play(int n, int k, ArrayList<Integer> actionNumber, Action action)
+    {
+        Scanner scanner = new Scanner(System.in);
+        while (n!=k) {
+
+            k = scanner.nextInt();
+            actionNumber.add(actionNumber.size(), k);
+            System.out.println(action.action(n,k));
+            System.out.println(actionNumber);
+            System.out.println(action.countNumber);
+
         }
     }
 }
